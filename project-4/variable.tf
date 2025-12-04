bf8e7e2a74fab91d4bfac92a20835c2d714dc533
Rahul Karmakar <rahulkarmakar@CCU-RKARMAKAR-MAC.local>
2025-12-04 23:58:40 +0530

    Created EKS Cluster using Terraform.

---
+++ project-4/variable.tf
@@ -0,0 +1,10 @@
+locals {
+   region = "us-west-2"
+   name = "eks-cluster-rk"
+   vpc_cidr = "10.0.0.0/16"
+   azs = ["us-west-2c","us-west-2a"]
+   private_subnets = ["10.0.1.0/24","10.0.2.0/24"]
+   public_subnets = ["10.0.101.0/24","10.0.102.0/24"]
+   intra_subnets = ["10.0.5.0/24","10.0.6.0/24"]
+   env = "Dev"
+}
